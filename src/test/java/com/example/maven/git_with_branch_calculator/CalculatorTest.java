002823965b9d69d079649f3b1ff679eb42d4a2e6
Madeleine Barwén <barwen@outlook.com>
2017-11-29 09:44:10 +0100

    I added the subtract method in Calculator (and already a test for it)

--- src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
+++ src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
@@ -32,17 +32,21 @@ private static final Logger LOG = Logger.getLogger(Calculator.class.getName());
 	}
 
 //	@Test
-//	public void testSubtractMethod() {
-//		Random random = new Random();
-//		int firstNumber = 0;
-//		int secondNumber = 0;
-//		int result = 0;
-//		
-//		firstNumber = random.nextInt(100);
-//		secondNumber = random.nextInt(100);
-//		
-//		result = firstNumber + secondNumber;
-//		LOG.info("Testing the method add with: " + firstNumber + " and " + secondNumber);
-//		assertEquals(cal.add(firstNumber, secondNumber), result);
-//	}
+	public void testSubtractMethod() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+		
+		for (int i = 0; i< 10; i++) {
+			firstNumber = random.nextInt(100);
+			secondNumber = random.nextInt(100);
+			
+			result = firstNumber - secondNumber;	
+			LOG.info("Testing the method subtract with: " + firstNumber + " and " + secondNumber);
+			assertEquals(cal.subtract(firstNumber, secondNumber), result);
+		}
+		
+		
+	}
 }
